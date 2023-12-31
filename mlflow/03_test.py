37187d5b84044589b7862dae228401d55f3234f4
rishabh013 <rishabhvishwakarma013@gmail.com>
2023-12-31 18:58:19 +0530

    add files

--- mlflow/03_test.py
+++ mlflow/03_test.py
@@ -4,7 +4,7 @@ import argparse
 
 if __name__ == '__main__':
     args=argparse.ArgumentParser()
-    args.add_argument("--name", "-n", default="sunny", type=str)
+    args.add_argument("--name", "-n", default="rishabh", type=str)
     args.add_argument("--age", "-a", default=25.0, type=float)
     parse_args=args.parse_args()
     
--- mlflow/04_run.py
+++ mlflow/04_run.py
@@ -7,4 +7,4 @@ max_depth =[20,25,15,10,5]
 
 for n in n_estimators:
     for m in max_depth:
-        os.system(f"python basic_ml_model.py -n{n} -m{m}")
+        os.system(f"python 02_basic_ml_model.py -n{n} -m{m}")
